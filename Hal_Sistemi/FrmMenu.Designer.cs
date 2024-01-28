1ffdf76719fdaeca643bad98f46a78682151c9a5
Mert Ağralı <100879484+mertagralii@users.noreply.github.com>
2024-01-28 05:22:54 +0300

    CariHareket tablo düzeni

--- Hal_Sistemi/FrmMenu.Designer.cs
+++ Hal_Sistemi/FrmMenu.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.CmbMusteri = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
@@ -37,6 +37,16 @@
             this.BtnCari = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
@@ -45,35 +55,45 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.CmbMusteri);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 36);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(849, 374);
+            this.groupBox1.Size = new System.Drawing.Size(460, 374);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Seçenekler";
             // 
-            // comboBox1
+            // CmbMusteri
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(136, 51);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(169, 32);
-            this.comboBox1.TabIndex = 1;
+            this.CmbMusteri.FormattingEnabled = true;
+            this.CmbMusteri.Location = new System.Drawing.Point(140, 36);
+            this.CmbMusteri.Name = "CmbMusteri";
+            this.CmbMusteri.Size = new System.Drawing.Size(169, 32);
+            this.CmbMusteri.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 52);
+            this.label1.Location = new System.Drawing.Point(6, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 24);
+            this.label1.Size = new System.Drawing.Size(128, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Cari(Müşteri):";
             // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(867, 36);
+            this.groupBox2.Location = new System.Drawing.Point(575, 36);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(418, 374);
             this.groupBox2.TabIndex = 1;
@@ -84,16 +104,16 @@
             // 
             this.groupBox3.Controls.Add(this.BtnUrunler);
             this.groupBox3.Controls.Add(this.BtnCari);
-            this.groupBox3.Location = new System.Drawing.Point(1307, 36);
+            this.groupBox3.Location = new System.Drawing.Point(1114, 36);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(251, 374);
+            this.groupBox3.Size = new System.Drawing.Size(380, 374);
             this.groupBox3.TabIndex = 1;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Diğer İşlemler";
             // 
             // BtnUrunler
             // 
-            this.BtnUrunler.Location = new System.Drawing.Point(27, 249);
+            this.BtnUrunler.Location = new System.Drawing.Point(95, 247);
             this.BtnUrunler.Name = "BtnUrunler";
             this.BtnUrunler.Size = new System.Drawing.Size(214, 53);
             this.BtnUrunler.TabIndex = 1;
@@ -103,7 +123,7 @@
             // 
             // BtnCari
             // 
-            this.BtnCari.Location = new System.Drawing.Point(27, 105);
+            this.BtnCari.Location = new System.Drawing.Point(95, 105);
             this.BtnCari.Name = "BtnCari";
             this.BtnCari.Size = new System.Drawing.Size(214, 53);
             this.BtnCari.TabIndex = 0;
@@ -116,7 +136,7 @@
             this.groupBox4.Controls.Add(this.dataGridView1);
             this.groupBox4.Location = new System.Drawing.Point(12, 416);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(1546, 290);
+            this.groupBox4.Size = new System.Drawing.Size(1482, 290);
             this.groupBox4.TabIndex = 2;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Veri Tabanı";
@@ -130,15 +150,101 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1540, 259);
+            this.dataGridView1.Size = new System.Drawing.Size(1476, 259);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(140, 105);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(169, 32);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ürün:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 162);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(169, 32);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Birim:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(140, 224);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(169, 32);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Birim Fiyatı:";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(140, 289);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(169, 32);
+            this.comboBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "KDV:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 337);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Toplam Fiyat:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(167, 337);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 24);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "0";
             // 
             // FrmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1583, 718);
+            this.ClientSize = new System.Drawing.Size(1517, 718);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
@@ -167,6 +273,16 @@
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CmbMusteri;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Hal_Sistemi/FrmMenu.cs
+++ Hal_Sistemi/FrmMenu.cs
@@ -25,29 +25,61 @@ namespace Hal_Sistemi
         void musterigetir()
         {
             baglanti.Open();
-            SqlCommand cari = new SqlCommand("Select ID,Unvan from TBLMusteri", baglanti);
+            SqlCommand cari = new SqlCommand("Select ID,Unvan from TBLMusteri Where SilindiMi = 0", baglanti);
             cari.CommandType = CommandType.Text;
             SqlDataReader dr = cari.ExecuteReader();
             while (dr.Read())
             {
 
-                comboBox1.Items.Add(dr["Unvan"]);
-                
+                CmbMusteri.Items.Add(dr["Unvan"]);
+
             }
             baglanti.Close();
         }
-       
+        // Ürün Getir
+        void ürüngetir()
+        {
+            baglanti.Open();
+            SqlCommand urun = new SqlCommand("Select ID,UrunAd from TBLUrun Where SilindiMi=0", baglanti);
+            urun.CommandType = CommandType.Text;
+            SqlDataReader dr = urun.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox2.Items.Add(dr["UrunAd"]);
+            }
+            baglanti.Close();
+        }
+        void listeleme()
+        {
+            // Listeleme Metodu
+            SqlDataAdapter da = new SqlDataAdapter("SELECT TBLUrun.ID AS 'Ürün Numarası',TBLMusteri.ID AS 'Müşteri Numarası',Tckn AS 'TCKN',Vkn AS 'Vergi Kimlik Numarası',EFatura AS 'E-Fatura' ,Unvan AS 'Ünvan' ,VergiDairesi,Adres,Telefon,UrunAd AS 'Ürün Adı',Birim,Cinsi,Mensei,BirimFiyat,KDV FROM TBLUrun INNER JOIN TBLMusteri\r\nON TBLUrun.ID = TBLMusteri.ID\r\n", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["Vergi Kimlik Numarası"].DisplayIndex = 3;
+            dataGridView1.Columns["Ünvan"].DisplayIndex = 4;
+            dataGridView1.Columns["E-Fatura"].DisplayIndex = 5;
+            dataGridView1.Columns["VergiDairesi"].DisplayIndex = 6;
+            dataGridView1.Columns["Adres"].DisplayIndex = 7;
+            dataGridView1.Columns["Telefon"].DisplayIndex = 8;
+            dataGridView1.Columns["Ürün Adı"].DisplayIndex = 9;
+            dataGridView1.Columns["Cinsi"].DisplayIndex = 10;
+            dataGridView1.Columns["Mensei"].DisplayIndex = 11;
+
+
+        }
 
         // CariHareket Tablosunu getirme
 
         void carihareket()
         {
             SqlCommand komut = new SqlCommand("Select * From TBLCariHareket Where MusteriID=@P1", baglanti);
-            komut.Parameters.AddWithValue("@P1", comboBox1.Text);
+            komut.Parameters.AddWithValue("@P1", CmbMusteri.Text);
             SqlDataAdapter da = new SqlDataAdapter(komut);
             DataTable dt = new DataTable();
             da.Fill(dt);
             dataGridView1.DataSource = dt;
+
         }
 
 
@@ -69,6 +101,8 @@ namespace Hal_Sistemi
         {
             musterigetir();
             carihareket();
+            ürüngetir();
+            listeleme();
 
         }
     }
